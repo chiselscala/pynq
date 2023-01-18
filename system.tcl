1b0de9aeb77483436dc47b47a50bca81faddf78b
chiselscala@gmail.com <chiselscala@gmail.com>
2023-01-19 00:59:38 +0800

    ported to tul_pynq_z2

--- system.tcl
+++ system.tcl
@@ -45,7 +45,11 @@ set script_folder [_tcl::get_script_folder]
 set list_projs [get_projects -quiet]
 if { $list_projs eq "" } {
    create_project ekiwi ekiwi -part xc7z020clg400-1
-   set_property BOARD_PART digilentinc.com:arty-z7-20:part0:1.0 [current_project]
+
+#                          digilentinc.com:arty-z7-20:part0:1.0 digilentinc.com:arty-z7-20:part0:1.1 
+#  set_property BOARD_PART  digilentinc.com:arty-z7-20:part0:1.0 [current_project]
+  set_property BOARD_PART tul.com.tw:pynq-z2:part0:1.0 [current_project]
+   #   set_property BOARD_PART digilentinc.com:arty-z7-20:part0:1.0 [current_project]
 }
 
 set_property  ip_repo_paths  chisel/ip [current_project]
